157c982ed0596fd1568f0ada8a658fb175a57c5c
nichoth <nichoth@gmail.com>
2016-03-01 15:47:02 -0800

    fix dep version

--- bin/publish.js
+++ bin/publish.js
@@ -1,7 +1,11 @@
 var ghpages = require('gh-pages');
 var path = require('path');
+var args = process.argv.slice(2);
+var repo = args[0] || 'https://github.com/nichoth/usurper.git';
+
 ghpages.publish(path.join(__dirname, '../public'), {
-  repo: 'https://github.com/nichoth/usurper.git'
-}, function() {
+  repo: repo
+}, function(err) {
+  if (err) return console.log(err);
   console.log("published");
 });

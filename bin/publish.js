36339c1710212300866c32e4c9a6ba6be86adbf4
nichoth <nichoth@gmail.com>
2016-03-01 16:14:50 -0800

    dont put cname on staging

--- bin/publish.js
+++ bin/publish.js
@@ -1,10 +1,13 @@
 var ghpages = require('gh-pages');
 var path = require('path');
 var args = process.argv.slice(2);
-var repo = args[0] || 'https://github.com/nichoth/usurper.git';
+var baseUrl = 'https://github.com/nichoth/usurper';
+var staging = args[0] === '--staging';
+var repo = staging ? baseUrl+'-staging.git' : baseUrl+'.git';
 
 ghpages.publish(path.join(__dirname, '../public'), {
-  repo: repo
+  repo: repo,
+  src: '**/*.*'
 }, function(err) {
   if (err) return console.log(err);
   console.log("published");
